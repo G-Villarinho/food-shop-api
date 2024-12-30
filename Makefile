d4b2b007f72076955347cbf971b8cbc99229cd3f
Gabriel Villarinho <gabrielvillarinho01@gmail.com>
2024-12-30 13:59:05 -0300

    Add key generation to Makefile and refactor token tests to use dynamic keys

--- Makefile
+++ Makefile
@@ -37,3 +37,19 @@ migration:
 	@echo "Rodando as migrações..."
 	go run database/migrations/main.go	
 
+generate-keys:
+	@if [ ! -f $(PRIVATE_KEY_FILE) ]; then \
+		echo "Generating ECDSA private key..."; \
+		openssl ecparam -genkey -name prime256v1 -noout -out $(PRIVATE_KEY_FILE); \
+		echo "Private key saved in $(PRIVATE_KEY_FILE)"; \
+	else \
+		echo "Private key already exists: $(PRIVATE_KEY_FILE)"; \
+	fi
+
+	@if [ ! -f $(PUBLIC_KEY_FILE) ]; then \
+		echo "Extracting public key from the private key..."; \
+		openssl ec -in $(PRIVATE_KEY_FILE) -pubout -out $(PUBLIC_KEY_FILE); \
+		echo "Public key saved in $(PUBLIC_KEY_FILE)"; \
+	else \
+		echo "Public key already exists: $(PUBLIC_KEY_FILE)"; \
+	fi
--- services/token_test.go
+++ services/token_test.go
@@ -1,184 +1,147 @@
 package services
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"testing"
 	"time"
 
 	"github.com/G-Villarinho/food-shop-api/config"
+	"github.com/G-Villarinho/food-shop-api/internal"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTokenService_CreateToken(t *testing.T) {
-	config.Env.PrivateKey = `-----BEGIN PRIVATE KEY-----
-								MIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQg5RehdfX0dB5a1b4O9LRW
-								GrasAxpShIkcufp95TfCCzyhRANCAASvtRoBafzVhFxPK2hnlLDrxIagEr/Eea7/
-								37M6Iqt9tfeF03AJcIAVw/QmtI0yuxsKoWMYbnlJoNtCwrvJ8HD0
-								-----END PRIVATE KEY-----`
-	config.Env.PublicKey = `-----BEGIN PUBLIC KEY-----
-								MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEr7UaAWn81YRcTytoZ5Sw68SGoBK/xHmu
-								/9+zOiKrfbX3hdNwCXCAFcP0JrSNMrsbCqFjGG55SaDbQsK7yfBw9A==
-								-----END PUBLIC KEY-----`
-	config.Env.Cache.SessionExp = 1
+	privateKey := `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIIlmFjHouHxEJrhWvTzdSDsMegE0sEDjd91Dy7DsDEUqoAoGCCqGSM49
+AwEHoUQDQgAEmSIeG8APOh4v1rX6ERinckdljyHErmfWPkwpNJOhOMD+8vKAfIbl
+dQpPG+XUKikw5UnzVTYP+uKN6kK3XHOTYg==
+-----END EC PRIVATE KEY-----`
 
-	tokenService := &tokenService{}
+	config.Env.PrivateKey = privateKey
+	config.Env.Cache.SessionExp = 6 // hour
+
+	tokenService := &tokenService{
+		di: internal.NewDi(),
+	}
 
 	t.Run("should create a valid token successfully", func(t *testing.T) {
 		userID := uuid.New()
 		sessionID := uuid.New()
 
 		token, err := tokenService.CreateToken(userID, sessionID)
-
 		assert.NoError(t, err)
 		assert.NotEmpty(t, token)
 
-		claims := jwt.MapClaims{}
-		parsedToken, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (any, error) {
-			return config.Env.PublicKey, nil
-		})
+		parsedToken, claims := validateToken(token, config.Env.PrivateKey, t)
 
-		assert.NoError(t, err)
 		assert.True(t, parsedToken.Valid)
 		assert.Equal(t, userID.String(), claims["userId"])
 		assert.Equal(t, sessionID.String(), claims["sid"])
 		assert.Equal(t, "level-up.com", claims["iss"])
-		assert.NotNil(t, claims["iat"])
-		assert.NotNil(t, claims["exp"])
 	})
 
-	t.Run("should return an error when signing fails", func(t *testing.T) {
-		// Simula chave inválida ao redefinir a assinatura do método `SignedString`
-		originalPrivateKey := config.Env.PrivateKey
-		defer func() {
-			config.Env.PrivateKey = originalPrivateKey // Restaura a chave válida após o teste
-		}()
-
-		// Define uma chave privada inválida como substituta temporária
-		config.Env.PrivateKey = "private_key" // Estrutura não inicializada
+	t.Run("should return error when private key is invalid", func(t *testing.T) {
+		invalidPrivateKey := `-----BEGIN EC PRIVATE KEY-----
+INVALID_KEY
+-----END EC PRIVATE KEY-----`
+		config.Env.PrivateKey = invalidPrivateKey
 
 		userID := uuid.New()
 		sessionID := uuid.New()
 
 		token, err := tokenService.CreateToken(userID, sessionID)
-
-		assert.Error(t, err) // Verifica se ocorreu o erro esperado
+		assert.Error(t, err)
 		assert.Empty(t, token)
 	})
 }
 
 func TestTokenService_ExtractSessionID(t *testing.T) {
-	// Configurar chave privada e pública para o teste
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	assert.NoError(t, err)
-	config.Env.PrivateKey = "privateKey"
-	config.Env.PublicKey = "PublicKey"
-	config.Env.Cache.SessionExp = 1 // 1 hora para expiração
+	privateKey := `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIIlmFjHouHxEJrhWvTzdSDsMegE0sEDjd91Dy7DsDEUqoAoGCCqGSM49
+AwEHoUQDQgAEmSIeG8APOh4v1rX6ERinckdljyHErmfWPkwpNJOhOMD+8vKAfIbl
+dQpPG+XUKikw5UnzVTYP+uKN6kK3XHOTYg==
+-----END EC PRIVATE KEY-----`
 
-	tokenService := &tokenService{}
+	publicKey := `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEmSIeG8APOh4v1rX6ERinckdljyHE
+rmfWPkwpNJOhOMD+8vKAfIbldQpPG+XUKikw5UnzVTYP+uKN6kK3XHOTYg==
+-----END PUBLIC KEY-----`
+
+	config.Env.PrivateKey = privateKey
+	config.Env.PublicKey = publicKey
+
+	tokenService := &tokenService{
+		di: internal.NewDi(),
+	}
 
 	t.Run("should extract session ID successfully from a valid token", func(t *testing.T) {
 		userID := uuid.New()
 		sessionID := uuid.New()
+		config.Env.Cache.SessionExp = 6
 
-		claims := jwt.MapClaims{
-			"userId": userID.String(),
-			"exp":    time.Now().Add(time.Hour).Unix(),
-			"sid":    sessionID.String(),
-			"iat":    time.Now().Unix(),
-			"iss":    "level-up.com",
-		}
-
-		token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-		signedToken, err := token.SignedString(privateKey)
-
+		token, err := tokenService.CreateToken(userID, sessionID)
 		assert.NoError(t, err)
 
-		extractedSessionID, err := tokenService.ExtractSessionID(signedToken)
-
+		extractedSessionID, err := tokenService.ExtractSessionID(token)
 		assert.NoError(t, err)
 		assert.Equal(t, sessionID, extractedSessionID)
 	})
 
-	t.Run("should return an error for invalid token signature", func(t *testing.T) {
+	t.Run("should return error when token is invalid", func(t *testing.T) {
+		invalidToken := "INVALID_TOKEN"
+
+		sessionID, err := tokenService.ExtractSessionID(invalidToken)
+		assert.Error(t, err)
+		assert.Equal(t, uuid.Nil, sessionID)
+	})
+
+	t.Run("should return error when public key is invalid", func(t *testing.T) {
+		config.Env.PublicKey = `-----BEGIN PUBLIC KEY-----
+INVALID_KEY
+-----END PUBLIC KEY-----`
+
 		userID := uuid.New()
 		sessionID := uuid.New()
 
-		claims := jwt.MapClaims{
-			"userId": userID.String(),
-			"exp":    time.Now().Add(time.Hour).Unix(),
-			"sid":    sessionID.String(),
-			"iat":    time.Now().Unix(),
-			"iss":    "level-up.com",
-		}
-
-		// Criar token com chave privada diferente
-		otherPrivateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		token, err := tokenService.CreateToken(userID, sessionID)
 		assert.NoError(t, err)
 
-		token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-		signedToken, err := token.SignedString(otherPrivateKey)
-
-		assert.NoError(t, err)
-
-		extractedSessionID, err := tokenService.ExtractSessionID(signedToken)
-
+		sessionID, err = tokenService.ExtractSessionID(token)
 		assert.Error(t, err)
-		assert.Equal(t, uuid.Nil, extractedSessionID)
+		assert.Equal(t, uuid.Nil, sessionID)
 	})
 
-	t.Run("should return an error for invalid session ID", func(t *testing.T) {
+	t.Run("should return error when session ID is missing in token", func(t *testing.T) {
 		claims := jwt.MapClaims{
 			"userId": uuid.New().String(),
 			"exp":    time.Now().Add(time.Hour).Unix(),
-			"sid":    "invalid-session-id", // Session ID inválido
 			"iat":    time.Now().Unix(),
 			"iss":    "level-up.com",
 		}
 
 		token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-		signedToken, err := token.SignedString(privateKey)
-
+		privateKey, err := parseECPrivateKey(config.Env.PrivateKey)
 		assert.NoError(t, err)
 
-		extractedSessionID, err := tokenService.ExtractSessionID(signedToken)
-
-		assert.Error(t, err)
-		assert.Equal(t, uuid.Nil, extractedSessionID)
-	})
-
-	t.Run("should return an error for expired token", func(t *testing.T) {
-		userID := uuid.New()
-		sessionID := uuid.New()
-
-		claims := jwt.MapClaims{
-			"userId": userID.String(),
-			"exp":    time.Now().Add(-time.Hour).Unix(), // Token expirado
-			"sid":    sessionID.String(),
-			"iat":    time.Now().Unix(),
-			"iss":    "level-up.com",
-		}
-
-		token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
 		signedToken, err := token.SignedString(privateKey)
-
 		assert.NoError(t, err)
 
-		extractedSessionID, err := tokenService.ExtractSessionID(signedToken)
-
+		sessionID, err := tokenService.ExtractSessionID(signedToken)
 		assert.Error(t, err)
-		assert.Equal(t, uuid.Nil, extractedSessionID)
+		assert.Equal(t, uuid.Nil, sessionID)
+	})
+}
+
+func validateToken(token, privateKey string, t *testing.T) (*jwt.Token, jwt.MapClaims) {
+	parsedKey, err := parseECPrivateKey(privateKey)
+	assert.NoError(t, err)
+
+	claims := jwt.MapClaims{}
+	parsedToken, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (any, error) {
+		return parsedKey.Public(), nil
 	})
 
-	t.Run("should return an error for malformed token", func(t *testing.T) {
-		malformedToken := "this-is-not-a-valid-token"
-
-		extractedSessionID, err := tokenService.ExtractSessionID(malformedToken)
-
-		assert.Error(t, err)
-		assert.Equal(t, uuid.Nil, extractedSessionID)
-	})
+	assert.NoError(t, err)
+	return parsedToken, claims
 }
